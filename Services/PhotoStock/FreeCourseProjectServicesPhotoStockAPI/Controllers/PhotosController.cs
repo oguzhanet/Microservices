3e09f13bc8b08fba6e43f2c0ad3183a2cf34d418
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-03-15 17:17:31 +0300

    15.03.2022

---
+++ Services/PhotoStock/FreeCourseProjectServicesPhotoStockAPI/Controllers/PhotosController.cs
@@ -0,0 +1,55 @@
+﻿using FreeCourseProjectServicesPhotoStockAPI.DTOs;
+using FreeCourseShared.Concrete;
+using FreeCourseShared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourseProjectServicesPhotoStockAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            string[] ValidImageFileTypes = { ".JPG", ".JPEG", ".PNG" };
+            bool isValidFileExtension = ValidImageFileTypes.Any(t => t == Path.GetExtension(photo.FileName).ToUpper());
+
+            if (photo != null && photo.Length > 0 && isValidFileExtension == true)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);
+
+                var returnPath = "photos/" + photo.FileName;
+
+                PhotoDto photoDto = new() { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
+        }
+
+        //[HttpPost]
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("photo not found", 404));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
---
+++ Services/PhotoStock/FreeCourseProjectServicesPhotoStockAPI/DTOs/PhotoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace FreeCourseProjectServicesPhotoStockAPI.DTOs
+{
+    public class PhotoDto
+    {
+        public string Url { get; set; }
+    }
+}
