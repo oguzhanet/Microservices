aed590537bc60b5992b14d37555ed0077ccc07e2
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-04-24 15:15:48 +0300

    24.04.2022

---
+++ Frontends/FreeCourseProjectWebUI/Controllers/BasketController.cs
@@ -0,0 +1,48 @@
+﻿using FreeCourseProjectWebUI.Models.Basket;
+using FreeCourseProjectWebUI.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourseProjectWebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+
+        public BasketController(ICatalogService catalogService, IBasketService basketService)
+        {
+            _catalogService = catalogService;
+            _basketService = basketService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var basket = await _basketService.GetAsync();
+            return View(basket);
+        }
+
+        public async Task<IActionResult> AddBasketItem(string courseId)
+        {
+            var course = await _catalogService.GetByCourseIdAsync(courseId);
+
+            var basketItem = new BasketItemViewModel
+            {
+                CourseId = course.Id,
+                CourseName = course.Name,
+                Price = course.Price
+            };
+
+            await _basketService.AddBasketItemAsync(basketItem);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> RemoveBasketItem(string courseId)
+        {
+            await _basketService.RemoveBasketItem(courseId);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
